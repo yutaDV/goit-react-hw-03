7415cf00ca90bc3c68c928d38b8330a37f8fe0e5
Yuliia <74568169+yutaDV@users.noreply.github.com>
2024-08-11 21:50:07 +0300

    search box

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 
-import './App.css'
-import ContactList from './components/ContactList/ContactList'
-import contacts from './contacts.json';
+import './App.css';
+import { useState } from 'react';
+import ContactList from './components/ContactList/ContactList';
+import SearchBox from './components/SearchBox/SearchBox';
+import contactsData from './contacts.json';
 
 function App() {
+  const [contacts, setContacts] = useState(contactsData);
+  const [searchQuery, setSearchQuery] = useState('');
 
+  const handleSearchChange = (query) => {
+    setSearchQuery(query);
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  console.log(contacts); // Перевірте, що контакти коректно імплементовані
 
   return (
     <>
       <div>
         <h1>Phonebook</h1>
-        {/* 
-  <ContactForm />
-        <SearchBox />
-  */}
-  <ContactList contacts={contacts} />
-</div>
-
+        <SearchBox value={searchQuery} onChange={handleSearchChange} />
+        <ContactList contacts={filteredContacts} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
